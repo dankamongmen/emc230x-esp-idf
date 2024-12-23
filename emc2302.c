d9cf8543774974f7de951d947a8ec146b201605e
nick black <dankamongmen@gmail.com>
2024-12-23 07:54:19 -0500

    detect both models of emc2302

--- emc2302.c
+++ emc2302.c
@@ -1,16 +1,54 @@
 #include "emc2302.h"
 #include <esp_log.h>
+#include <stdint.h>
 #include <freertos/FreeRTOS.h>
 
 static const char* TAG = "emc";
 
-#define EMC2302_ADDRESS 0x2A
+#define EMC2302_1_ADDRESS 0x2e // emc2302-1
+#define EMC2302_2_ADDRESS 0x2f // emc2302-2
 
-int emc2302_detect(i2c_master_bus_handle_t i2c, i2c_master_dev_handle_t* i2cnau){
-  const unsigned addr = EMC2302_ADDRESS;
+// probe for an emc2302 at the specified address
+static int
+emc2302_mod_detect(i2c_master_bus_handle_t i2c, uint8_t addr){
   esp_err_t e = i2c_master_probe(i2c, addr, TIMEOUT_MS);
   if(e != ESP_OK){
-    ESP_LOGE(TAG, "error %d detecting EMC2302 at 0x%02x", e, addr);
+    ESP_LOGI(TAG, "didn't detect EMC2302 at 0x%02x", addr);
+    return -1;
+  }
+  return 0;
+}
+
+static int16_t
+emc2302_detect_addr(i2c_master_bus_handle_t i2c, emc2302_model model){
+  switch(model){
+    case EMC2302_MODEL_UNSPEC:
+      if(emc2302_mod_detect(i2c, EMC2302_1_ADDRESS) == 0){
+        return EMC2302_1_ADDRESS;
+      }
+      if(emc2302_mod_detect(i2c, EMC2302_2_ADDRESS) == 0){
+        return EMC2302_2_ADDRESS;
+      }
+      break;
+    case EMC2302_MODEL_1:
+      if(emc2302_mod_detect(i2c, EMC2302_1_ADDRESS) == 0){
+        return EMC2302_1_ADDRESS;
+      }
+      break;
+    case EMC2302_MODEL_2:
+      if(emc2302_mod_detect(i2c, EMC2302_2_ADDRESS) == 0){
+        return EMC2302_2_ADDRESS;
+      }
+      break;
+  }
+  return -1;
+}
+
+int emc2302_detect(i2c_master_bus_handle_t i2c, i2c_master_dev_handle_t* i2cemc,
+                   emc2302_model model){
+  int16_t addr = emc2302_detect_addr(i2c, model);
+  if(addr < 0){
+    ESP_LOGE(TAG, "error detecting EMC2302");
     return -1;
   }
   i2c_device_config_t devcfg = {
@@ -19,7 +57,7 @@ int emc2302_detect(i2c_master_bus_handle_t i2c, i2c_master_dev_handle_t* i2cnau)
     .scl_speed_hz = 100000,
 	};
   ESP_LOGI(TAG, "successfully detected EMC2302 at 0x%02x", addr);
-  if((e = i2c_master_bus_add_device(i2c, &devcfg, i2cnau)) != ESP_OK){
+  if((e = i2c_master_bus_add_device(i2c, &devcfg, i2cemc)) != ESP_OK){
     ESP_LOGE(TAG, "error %d adding emc2302 i2c device", e);
     return -1;
   }
--- include/emc2302.h
+++ include/emc2302.h
@@ -5,9 +5,18 @@
 
 #include <driver/i2c_master.h>
 
+typedef enum {
+  EMC2302_MODEL_UNSPEC,
+  EMC2302_MODEL_1,
+  EMC2302_MODEL_2,
+} emc2302_model;
+
 // probe the I2C bus for an EMC2302. if it is found, configure i2cemc
 // as a device handle for it, and return 0. return non-zero on error.
-int emc2302_detect(i2c_master_bus_handle_t i2c, i2c_master_dev_handle_t* i2cemc);
+// specify the model to match only that one, or _UNSPEC to probe for
+// all known models (this can lead to false positives).
+int emc2302_detect(i2c_master_bus_handle_t i2c, i2c_master_dev_handle_t* i2cemc,
+                   emc2302_model model);
 
 // send the reset command w/ timeout. returns non-zero on error.
 int emc2302_reset(i2c_master_dev_handle_t i2c);
